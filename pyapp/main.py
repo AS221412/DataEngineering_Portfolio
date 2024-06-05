b3e3de219611a9385afbb0f4098e75a7ae2e3a74
AS221412 <alizadeh1412@gmail.com>
2024-06-05 21:20:49 +0200

    batches geändert

--- pyapp/main.py
+++ pyapp/main.py
@@ -10,7 +10,7 @@ collection = db["co2_data"]
 
 
 #Reading CSV File and storing it in batchesto the collection 
-chunk_size = 500
+chunk_size = 50
 for chunk in pd.read_csv("iot_telemetry_data.csv", chunksize=chunk_size):
     # Daten in MongoDB einfügen
     records = chunk.to_dict(orient="records")
